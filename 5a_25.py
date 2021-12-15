d36720f016882f269bda8afd30a5ccb931901a00
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-15 16:39:14 +1000

    Add files via upload

---
+++ 5a_25.py
@@ -0,0 +1,25 @@
+from pingouin import welch_anova
+
+# BAR PLOTS OF THREE GROUPS FOR A VARIABLE
+
+# Subset three groups
+group1 = df[df['productionlocation'] == 'USA']['rating']
+group2 = df[df['productionlocation'] == 'Coproduction']['rating']
+group3 = df[df['productionlocation'] == 'Non-USA']['rating']
+
+means = (group1.mean(), group2.mean(), group3.mean())  # Calculating means
+positions = [0, 1, 2]  # Defining positions in the graph
+plt.bar(positions, means)  # Compiling the plot
+plt.xticks(positions, ['USA', 'Coproduction', 'Non-USA'],
+           rotation="horizontal")  # Adding labels
+plt.tight_layout()  # Forces tidy plot lay-out
+plt.savefig("barmeanstd3+.pdf")  # Save figure
+plt.clf()  # Clear figure
+
+# Get descriptive table by category
+print(rp.summary_cont(df['rating'].groupby(df['productionlocation'])))
+pd.set_option('max_columns', 9999)
+
+# Welch's ANOVA
+aov = welch_anova(dv='rating', between='productionlocation', data=df)
+print(aov)
