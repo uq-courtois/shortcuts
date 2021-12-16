c8cafcdb38c071f87749d358a5acb28a50991306
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-16 15:10:16 +1000

    Update 1a_05b.py

--- 1a_05b.py
+++ 1a_05b.py
@@ -1,9 +1,7 @@
 z = ['Lisa','Marc',4,5,'Jing'] # List with mixed data
 
-# Print from the second element (index 1)
-# to the last element, including the second element
+# Print from the second element (index 1) to the last element
 print(z[1:])
 
-# Print up until, but not including, third element
-# (index 2)
+# Print up until, but not including, third element (index 2)
 print(z[:2]) 
