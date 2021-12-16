200e83d6e4d135b1196dbda228800bab0069576c
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-16 15:05:18 +1000

    Update 1a_05i.py

--- 1a_05i.py
+++ 1a_05i.py
@@ -1,6 +1,7 @@
 x = ['Jing','Kate','Lisa']
 y = ['Eric','Joan']
 
-# z is now a list that contains all the values from x and y
+# z is now a list that contains 
+# all the values from x and y
 z = x + y 
 print(z)
