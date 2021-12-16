c3a68c160997d6d0c3c232b69929e7deebb4b253
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-16 14:16:19 +1000

    Update 1a_03b.py

--- 1a_03b.py
+++ 1a_03b.py
@@ -1,12 +1,15 @@
 x = 'This is'
 y = 'a sentence'
 
-z = x + y # Concatenating two string variables
+# Concatenating two string variables
+z = x + y 
 print(z)
 
-z = x + ' ' + y # Concatenating two string variables and a string
+# Concatenating two string variables and a string
+z = x + ' ' + y 
 print(z)
 
-# Concatenating a string variable, a string, an integer (that has been converted to a string) and a string
+# Concatenating a string variable, a string, an integer 
+# (that has been converted to a string) and a string
 z = x + ' ' + str(4) + ' words'
 print(z)
