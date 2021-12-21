a765ef7b88db994d9d31112471304512d1e381ed
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-21 16:29:06 +1000

    Create 1b_05b.py

---
+++ 1b_05b.py
@@ -0,0 +1,33 @@
+# A list 
+# Iterator value = element
+x = [1,2,3,4]
+
+for i in x:
+	print(i)
+
+# A list of dictionaries
+# Iterator value = element = dictionary
+
+x = [
+	{'Name':'Jamie','Age':19,},
+	{'Name':'Ella','Age':21},
+]
+
+for i in x:
+	print(i)
+
+# A dictionary
+# Iterator value = key
+x = {'Name':'Jamie','Age':19,}
+
+for i in x:
+	print(i)
+
+# A string
+# Iterator value = character in string
+x = 'This is a sentence'
+
+for i in x:
+	print(i)
+
+# ...
