ef6139cf440a186248c89f5eabf17af94d3d5069
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-21 16:36:22 +1000

    Update 1b_05b.py

--- 1b_05b.py
+++ 1b_05b.py
@@ -1,27 +1,33 @@
 # A list 
-# Iterator value = element
+# Iterator value = element in list
 x = [1,2,3,4]
 
 for i in x:
 	print(i)
 
 # A list of dictionaries
-# Iterator value = element = dictionary
+# Iterator value = element in list = dictionary
 
 x = [
-	{'Name':'Jamie','Age':19,},
+	{'Name':'Jamie','Age':19},
 	{'Name':'Ella','Age':21},
 ]
 
 for i in x:
 	print(i)
+	
+	# We can access a value by adding a key:
+	print(i['Name'])
 
 # A dictionary
-# Iterator value = key
-x = {'Name':'Jamie','Age':19,}
+# Iterator value = dictonary key
+x = {'Name':'Jamie','Age':19}
 
 for i in x:
 	print(i)
+	
+	#We can even access each value:
+	print(x[i])
 
 # A string
 # Iterator value = character in string
