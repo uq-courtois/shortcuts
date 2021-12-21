cfd8a103998245e0325c12a9e78020f371e1ed91
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-21 15:38:13 +1000

    Update 1a_05m.py

--- 1a_05m.py
+++ 1a_05m.py
@@ -1,6 +1,7 @@
 x = ['w','o','r','d']
 
-# The list will be joint together using with the string within '' in between the elements
+# The list will be joint together using with 
+# the string within '' in between the elements
 y = ''.join(x)
 print(y)
 y = '-'.join(x)
