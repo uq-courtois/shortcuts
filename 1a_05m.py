aa3b150b95af52890789a6a155464829fa41243a
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-21 15:34:52 +1000

    Create 1a_05m.py

---
+++ 1a_05m.py
@@ -0,0 +1,9 @@
+x = ['w','o','r','d']
+
+# The list will be joint together using with the string within '' in between the elements
+y = ''.join(x)
+print(y)
+y = '-'.join(x)
+print(y)
+y = ', '.join(x)
+print(y)
