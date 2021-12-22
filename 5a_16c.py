76566bf5fe9177c7db9eda2fb8ec32b322bc36e5
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 15:04:42 +1000

    Update 5a_16c.py

--- 5a_16c.py
+++ 5a_16c.py
@@ -1,1 +1,4 @@
+import pandas as pd # import once at the top of your script
+
 x = df.groupby(['y'], as_index=False)[['z']].median()
+# x is a new dataframe with the median value of z per unique value of y
