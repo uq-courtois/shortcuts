25c37d8552713705c4aab1364ccf38856b303d08
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 14:47:14 +1000

    Update 5a_03.py

--- 5a_03.py
+++ 5a_03.py
@@ -1,3 +1,5 @@
+import pandas as pd # import once at the top of your script
+
 # You just need to replace the column name in the ''
-# Get overview of the first 5 and last 5 values of a variable
-print(df['recommended_artist'])
+# Get overview of the first 5 and last 5 values of variable/column 'x'
+print(df['x'])
