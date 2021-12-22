5399d8a1d2174bc39ee87d29e8c6b689107c381d
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 14:54:42 +1000

    Update 5a_04.py

--- 5a_04.py
+++ 5a_04.py
@@ -1,3 +1,5 @@
-# You just need to replace the column name in the ''
-# Get overview of more than one variable
-print(df[['recommended_artist', 'recommended_track']])
+import pandas as pd # import once at the top of your script
+
+# You just need to replace the column names in the ''
+# Get overview of more than one variable: x and y
+print(df[['x', 'y']])
