47f45586193089b32462c619a4438a83ca8fc2d0
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 14:57:49 +1000

    Update 5a_12a.py

--- 5a_12a.py
+++ 5a_12a.py
@@ -1,3 +1,5 @@
+import pandas as pd # import once at the top of your script
+
 df['y'] = df['x'] + 5
 # A new variable y is created where the values are the values of x + 5
 # Could be any operator: *, - , / , etc.
