ef59305abb5730f8427bea28f36d0c978cecaf97
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 15:19:40 +1000

    Update 5a_22a.py

--- 5a_22a.py
+++ 5a_22a.py
@@ -1,12 +1,15 @@
-# STACKED BAR CHART
-ct = pd.crosstab(df['productionlocation'], df['sfxgenre'],
+import pandas as pd # import once at the top of your script
+import researchpy as rp # import once at the top of your script
+
+# STACKED BAR CHART for variables/columns x and y
+ct = pd.crosstab(df['x'], df['y'],
                  normalize='index')  # Create PD crosstab as basis for plot
 ct.plot.bar(stacked=True)  # Generate plot
 plt.legend(loc='lower left', bbox_to_anchor=(0.0, 1.01),
            frameon=False)  # Tidy up legend (other)
 plt.xticks(rotation="horizontal")  # Horizontal x-axis labels
-plt.xlabel('Production location')  # Adding label on x-axis
-plt.ylabel('Proportion SFX')  # Adding label on y-axis
+plt.xlabel('Enter label for x-axis here')  # Adding label on x-axis
+plt.ylabel('Enter label of y-axis here')  # Adding label on y-axis
 plt.tight_layout()  # Forces tidy plot lay-out
 plt.savefig('stackedbar.pdf')  # Saving figure
 
