4dd0334ec8cd4db3105a7ea5f65e53fb64c70165
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 14:59:17 +1000

    Update 5a_15.py

--- 5a_15.py
+++ 5a_15.py
@@ -1,11 +1,11 @@
+import pandas as pd # import once at the top of your script
+
 # DF before
 print('\nBefore', df)
-
 
 def age_cal(date_of_birth):
     age = 2022 - date_of_birth
     return age
-
 
 df['age'] = df['date_of_birth'].apply(lambda x: age_cal(x))
 
