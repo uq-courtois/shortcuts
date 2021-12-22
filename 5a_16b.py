6d3230126445550a80845bf5e7e71688309fa9a6
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 15:00:44 +1000

    Update 5a_16b.py

--- 5a_16b.py
+++ 5a_16b.py
@@ -1,1 +1,4 @@
+import pandas as pd # import once at the top of your script
+
 x = df.groupby(['y'], as_index=False)[['z']].max()
+# x is a new dataframe with the maximum value of z per unique value of y
