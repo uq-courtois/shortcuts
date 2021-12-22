91fea89a184cbee715bc023862fcc125afa29b9a
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 14:56:06 +1000

    Update 5a_07.py

--- 5a_07.py
+++ 5a_07.py
@@ -1,2 +1,5 @@
+import pandas as pd # import once at the top of your script
+
 # Sort values of x in descending order
+# To sort in ascending order: change False into True
 df = df.sort_values(by=['x'], ascending=False)
