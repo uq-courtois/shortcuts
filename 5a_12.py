83b7126d1c3cf370f788e0fb98e7c85accd3fe96
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 14:57:35 +1000

    Update 5a_12.py

--- 5a_12.py
+++ 5a_12.py
@@ -1,3 +1,5 @@
+import pandas as pd # import once at the top of your script
+
 df['x'] = df['x'] * 5
 # values of x are overwritten with the values of x * 5
 # Could be any operator: +, - , / , etc.
