c8d330e544136b1fcf9ab7718bf08df7ea0cc7f3
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 15:59:22 +1000

    Update 5a_26a.py

--- 5a_26a.py
+++ 5a_26a.py
@@ -3,8 +3,8 @@ import pandas as pd # import once at the top of your script
 df1 = pd.read_csv('file1.csv',sep=';') # Read file 1
 df2 = pd.read_csv('file2.csv',sep=';') # Read file 2
  
-print(df1.shape) # Dimensions df1
-print(df2.shape) # Dimensions df2
+print(df1.info()) # Info for df1
+print(df2.info()) # Info for df2
  
 df = df1.append(df2) # Adding df1 and df2 together
  
