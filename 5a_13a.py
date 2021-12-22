7a634cc0829545ca3633a46fc468a1d41fe1651e
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 14:58:38 +1000

    Update 5a_13a.py

--- 5a_13a.py
+++ 5a_13a.py
@@ -1,2 +1,4 @@
-# Grabs the fifth, fourth, third, and second last digits and saves as new variable
+import pandas as pd # import once at the top of your script
+
+# Grabs the fifth, fourth, third, and second last digits from each value of 'year' and saves as new variable 'release_year'
 df['year'] = df['release_year'].str[-5:-1]
