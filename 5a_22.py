bf5c147703e535386bbbfa13aad419ad2b622075
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 15:14:15 +1000

    Update 5a_22.py

--- 5a_22.py
+++ 5a_22.py
@@ -1,9 +1,12 @@
+import pandas as pd # import once at the top of your script
+import researchpy as rp # import once at the top of your script
+
 # Set max of rows to show, in/decrease to needs
 pd.set_option('max_rows', 9999)
 
-# CROSSTABULATION
+# CROSSTABULATION of categorical variables x and y
 table, results = rp.crosstab(
-    df['sfxgenre'], df['productionlocation'], prop='col', test='chi-square')
+    df['x'], df['y'], prop='col', test='chi-square')
 
 print(table)  # Prints crosstab
 print()
