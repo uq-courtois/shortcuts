7055345f78331e812ddab66920fb0f65ca72b60b
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 15:21:12 +1000

    Update 5a_23a.py

--- 5a_23a.py
+++ 5a_23a.py
@@ -1,7 +1,10 @@
-# SCATTERPLOT
-plt.scatter(df['agefilm'], df['rating'])  # Build plot
-plt.xlabel('Age of Movie')  # x-axis label
-plt.ylabel('IMDB user rating')  # y-axis label
+import matplotlib.pyplot as  # import once at the top of your script
+import pandas as pd # import once at the top of your script
+
+# SCATTERPLOT for variables x and y
+plt.scatter(df['x'], df['y'])  # Build plot
+plt.xlabel('Enter label of x-axis here')  # x-axis label
+plt.ylabel('Enter label of y-axis here')  # y-axis label
 plt.tight_layout()  # Forces tidy plot lay-out
 plt.savefig('scatterplot.pdf')  # Save figure
 plt.clf()  # Clear figure
