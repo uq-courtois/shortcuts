22f6acb398c5bcabdff276aa4f82bd0a64390e93
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 14:54:15 +1000

    Update 5a_03b.py

--- 5a_03b.py
+++ 5a_03b.py
@@ -1,2 +1,4 @@
-# Get overview of the last 10 values of a variable:
-print(df['recommended_artist'].tail(10))
+import pandas as pd # import once at the top of your script
+
+# Get overview of the last 10 values of variable/column:
+print(df['x'].tail(10))
