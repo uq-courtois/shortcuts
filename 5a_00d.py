7f5f132fc1a2a320aa2992d64d79cd9d1cd1e092
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 16:40:28 +1000

    Update 5a_00d.py

--- 5a_00d.py
+++ 5a_00d.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-# Will show maximum 9999 columns in an output - All stacked underneath each other 
+# Will show maximum 9999 columns in an output 
+# - All stacked underneath each other 
 # This is more than the replit console can show
 pd.set_option('max_columns', 9999)
