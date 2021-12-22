4358d3e59c85d405d2f2780d16dc53cbf3257ae2
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 15:05:48 +1000

    Update 5a_16d.py

--- 5a_16d.py
+++ 5a_16d.py
@@ -1,1 +1,4 @@
-x = df.groupby(['y'], as_index=False)[['z']].sd()
+import pandas as pd # import once at the top of your script
+
+x = df.groupby(['y'], as_index=False)[['z']].std()
+# x is a new dataframe with the standard deviation of the observations in z for every unique value of y
