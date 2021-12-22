1d60054bdc389790863573e4f43e980cb07a3c95
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 16:36:19 +1000

    Update 5a_00c.py

--- 5a_00c.py
+++ 5a_00c.py
@@ -1,2 +1,4 @@
 import pandas as pd
-pd.set_option('max_rows', 9999) # Will show a maximum of 9999 rows (which is more than replit can show)
+# Will show a maximum of 9999 rows
+# (which is more than replit can show)
+pd.set_option('max_rows', 9999)
