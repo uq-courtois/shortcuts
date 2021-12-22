0a759f7bc494d0616f674eec1cd8f5e85e79c0cf
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 14:59:04 +1000

    Update 5a_14.py

--- 5a_14.py
+++ 5a_14.py
@@ -1,6 +1,7 @@
+import pandas as pd # import once at the top of your script
+
 # DF before
 print('\nBefore', df)
-
 
 def categorisation(headline):
     if 'COVID' in headline:
@@ -8,7 +9,6 @@ def categorisation(headline):
     else:
         covid_mention = 'no'
     return covid_mention
-
 
 df['covid_mention'] = df['headline'].apply(lambda x: categorisation(x))
 
