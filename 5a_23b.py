20bf862c446ec4c9d4f3555349929e2895b28295
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 15:22:20 +1000

    Update 5a_23b.py

--- 5a_23b.py
+++ 5a_23b.py
@@ -1,12 +1,18 @@
+import pandas as pd # import once at the top of your script
+import matplotlib.pyplot as  # import once at the top of your script
+
 # Displaying time series
 
 # Calculate mean, minimum, and maximum ratings per year (groupby)
+# Here, 4 aggregates are created, meant to draw 4 plot lines
+# Each aggregate is a different statistic: mean, median, min, max
 ymean = df.groupby('year', as_index=False)['rating'].mean()
 ymdn = df.groupby('year', as_index=False)['rating'].median()
 ymin = df.groupby('year', as_index=False)['rating'].min()
 ymax = df.groupby('year', as_index=False)['rating'].max()
 
 # Plot these variables together
+# Here,the 4 plot lines are added
 plt.plot(ymin['year'], ymin['rating'], label="Minimum rating")
 plt.plot(ymean['year'], ymean['rating'], label="Average rating")
 plt.plot(ymdn['year'], ymdn['rating'], label="Median rating")
