25d3a772cb67580de47c952ca51a3d0492a7a0bf
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 14:59:32 +1000

    Update 5a_16.py

--- 5a_16.py
+++ 5a_16.py
@@ -1,2 +1,4 @@
+import pandas as pd # import once at the top of your script
+
 x = df.groupby(['name'], as_index=False)[['expenditure']].sum()
 # x is a new dataframe with summed values for variable 'expenditure' per unique value in variable 'name'
