e5e6ad0f312f40c6d561c0f8d8a6cb5d0e2ff9e9
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2021-12-22 15:20:13 +1000

    Update 5a_23.py

--- 5a_23.py
+++ 5a_23.py
@@ -1,5 +1,7 @@
-# CORRELATION
-print(rp.correlation.corr_pair(df[['agefilm', 'rating']]))
+import researchpy as rp # import once at the top of your script
+
+# CORRELATION between numeric variables.columns x and y
+print(rp.correlation.corr_pair(df[['x', 'y']]))
 
 # The r-value is the magnitude of the correlation
 # p-value expresses statistical significance level
