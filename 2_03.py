592d71b6a96ee25c3e618d6cf82d83890f078203
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2022-03-24 11:59:25 +1000

    Update 2_03.py

--- 2_03.py
+++ 2_03.py
@@ -1,4 +1,4 @@
 import pandas as pd # import pandas module, once at the top of the script
 
-data = pd.DataFrame(newdata) # Converting list of dictionaries into dataframe
+data = pd.DataFrame(data) # Converting list of dictionaries into dataframe
 data.to_csv('file.csv',sep=',',index=False) # Writing dataframe into file.csv
