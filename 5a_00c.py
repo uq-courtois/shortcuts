6bcfac21650d358eb3eb4e9826ea70b3641609c7
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2022-05-02 14:57:01 +1000

    Update 5a_00c.py

--- 5a_00c.py
+++ 5a_00c.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 # Will show maximum 9999 rows in an output 
 # This is more than the replit console can show
-pd.set_option('max_rows', 9999)
+pd.set_option('display.max_rows', 9999)
