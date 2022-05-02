68820e2659b117a91c468c7a5b547ee380a01d2a
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2022-05-02 15:04:09 +1000

    Update 5a_01.py

--- 5a_01.py
+++ 5a_01.py
@@ -2,3 +2,7 @@ import pandas as pd # import once at the top of your script
 
 # Get overview of the outer variables and values of df
 print(df)
+
+# Get an overall shape of the dataframe
+# This is the number of rows and columns
+print(df.shape)
