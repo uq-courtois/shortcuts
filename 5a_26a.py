9875ea0c7e54e508b197992a121293c760d4a39b
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2022-05-03 18:03:28 +1000

    Update 5a_26a.py

--- 5a_26a.py
+++ 5a_26a.py
@@ -1,7 +1,7 @@
 import pandas as pd # import once at the top of your script
 
-df1 = pd.read_csv('file1.csv',sep=';') # Read file 1
-df2 = pd.read_csv('file2.csv',sep=';') # Read file 2
+df1 = pd.read_csv('file1.csv',sep=',') # Read file 1
+df2 = pd.read_csv('file2.csv',sep=',') # Read file 2
  
 print(df1.info()) # Info for df1
 print(df2.info()) # Info for df2
