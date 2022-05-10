bb2d492859e892ac7a6e6abd1eedfcd8a4120c3c
uq-courtois <60864284+uq-courtois@users.noreply.github.com>
2022-05-10 18:11:37 +1000

    Update 5a_22.py

--- 5a_22.py
+++ 5a_22.py
@@ -2,7 +2,7 @@ import pandas as pd # import once at the top of your script
 import researchpy as rp # import once at the top of your script
 
 # Set max of rows to show, in/decrease to needs
-pd.set_option('max_rows', 9999)
+pd.set_option('display.max_rows', 9999)
 
 # CROSSTABULATION of categorical variables x and y
 table, results = rp.crosstab(
